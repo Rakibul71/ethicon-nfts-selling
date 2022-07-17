c4dd5adffe2114592627d87f07bc6c05cad32fc9
Rakibul71 <rakibulislam202019@gmail.com>
2022-07-17 20:00:04 +0600

    min nfts design commit

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,14 +1,17 @@
 import Analytics from "./Analytics";
 import Hero from "./Hero";
+import MintNfts from "./MintNfts";
 
 
 const Dashboard = () => {
+
     return (
         <>
             <main className="h-screen overflow-y-scroll scroll-style">
                 <div className="w-10/12 lg:w-11/12 mr-auto ml-auto mt-[13vh]">
                     <Hero />
                     <Analytics totalNFTsSupply={'100'} totalClaimedSupply={'50'} totalUnClaimedSupply={'50'} />
+                    <MintNfts />
                 </div>
             </main>
         </>
